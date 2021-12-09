ba8eb23aec9c722abcdd8f63b26ea7b9e8fc19c4
wei-enwang <warrenwang6303@gmail.com>
2021-12-09 00:56:42 -0500

    add logistic regression

---
+++ log_reg.py
@@ -0,0 +1,20 @@
+import joblib
+import numpy as np
+
+import models
+
+
+seed = 32
+np.random.seed(seed)
+
+# use one dataset for now
+train_data_dir = "./data/enron1/"
+test_data_dir = "./data/enron2/"
+output_dir = "./output/"
+
+
+model = models.LogReg(train_data_dir, use_tfidf=True)
+model.fit()
+model.test(test_data_dir)
+
+# joblib.dump(model, output_dir+"_log_reg.pkl")
